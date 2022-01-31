ad0ac3d5e40609dc72a503d26c1806dd9c80235e
Alberto <abuenocr21dw@ikzubirimanteo.com>
2022-01-31 11:40:03 +0100

    alumno

---
+++ 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/Proyecto1/src/Alumno.java
@@ -0,0 +1,23 @@
+
+public class Alumno {
+	
+
+		private String dni;
+		private String nombre;
+		private String apellido;
+		private Ciclo ciclo;
+		private Notas notas;
+		
+		public Alumno(String dni, String nombre, String apellido, Ciclo ciclo, Notas notas) {
+
+			this.dni=dni;
+			this.nombre=nombre;
+			this.apellido=apellido;
+			this.notas=notas;
+			this.ciclo=ciclo;
+			
+			
+		}
+
+
+}
